23b56d2ea4cb0294bb6d1bb8fdf23934765b325a
Yorumoto Akira <yorumoto.akira@outlook.com>
2022-03-27 07:33:42 +0200

    build: 6.2.211

---
+++ release-force.ps1
@@ -0,0 +1,14 @@
+Write-Output "Did you check..."
+Write-Output "1) That you are on master?"
+Write-Output "2) That you are up to date with origin?"
+Write-Output "3) That the changelog is updated?"
+Write-Output "4) That the assembly version is updated?"
+Write-Output "5) That you specified the correct assembly version when running this script?"
+Read-Host
+
+$version = $args[0]
+
+git add .
+git commit --amend -m "build: ${version}"
+git tag -a -s -f -m "This is XIVLauncher build ${version}" "${version}"
+git push -f origin test/new-squirrel ${version}
